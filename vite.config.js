399c28716461b62d0c98ddf5a87a844fa9653303
TAKOU-24 <takoudimitri@yahoo.fr>
2026-08-10 21:56:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        inscription: resolve(__dirname, "inscription.html"),
+        merciInscription: resolve(__dirname, "merci-inscription.html"),
+      },
+    },
+  },
 });
